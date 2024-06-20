fbab253c360a5dd38824be9d5dba8fce716cd1e6
Chiragbhatt711 <chiragbhatt675@gmail.com>
2024-06-20 23:08:21 +0530

    user changes

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -129,4 +129,60 @@ class UserController extends Controller
         return redirect()->route('admin.users.index')
                         ->with('success',__('User delete successfully'));
     }
+
+    public function manageUser($id)
+    {
+        $users = User::orderBy('users.id','ASC')->where(['users.admin_id'=>$id])
+        ->join('roles','users.role_id','=','roles.id')
+        ->select('users.*','roles.name as role')
+        ->paginate(10);
+
+        return view('admin.users.user_list',compact('users','id'));
+    }
+
+    public function createUser($id)
+    {
+        $roles = Role::orderBy('id','ASC')->where('admin_id',$id)->get()->pluck('name','id')->toArray();
+
+        return view('admin.users.user_create',compact('roles','id'));
+    }
+
+    public function userStore(Request $request,$id)
+    {
+        $this->validate($request, [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|unique:users,email',
+            'mobile_number' => 'required',
+            'role_id' => 'required',
+            'password'=> 'required',
+        ],[
+            'first_name.required'=> 'Please enter first name',
+            'last_name.required' => 'Please enter last name',
+            'email.required' => 'Please enter email',
+            'mobile_number.required' => 'Pleaseb enter mobile number',
+            'role_id.required' => 'Please select role',
+            'password.required' => 'Please enter password',
+        ]);
+
+        $admin_id = $id;
+
+        $input = $request->all();
+        if(isset($input['password']) && $input['password'])
+        {
+            $input['password'] = Hash::make($input['password']);
+        }
+        $input['admin_id'] = $admin_id;
+        $input['is_active'] = 1;
+        $input['name'] = $input['first_name'].' '.$input['last_name'];
+
+        $user = User::create($input);
+        $role = Role::where('id', $request->input('role_id'))->select('name')->first()->toArray();
+        $guardName = 'web';
+        $role = Role::findByName($role['name'], $guardName);
+        $user->assignRole($role);
+        // $user->assignRole($role['name'],['guard_name' => 'web']);
+
+        return redirect()->route('admin.manage_user',$id)->with('success','User create successfully');
+    }
 }
